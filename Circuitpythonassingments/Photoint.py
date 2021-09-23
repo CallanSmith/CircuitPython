056dc5e6146c5cfc2b7fe61307b646297a97911d
Callan Smith <csmith29@ccs.local>
2021-09-23 10:04:51 -0400

    Adding photoint assingment to github

---
+++ Circuitpythonassingments/Photoint.py
@@ -0,0 +1,27 @@
+from digitalio import DigitalInOut, Direction, Pull
+import time
+import board
+
+interrupter = DigitalInOut(board.D7)
+interrupter.direction = Direction.INPUT
+interrupter.pull = Pull.UP
+
+counter = 0
+
+photo = False
+state = False
+
+max = 4
+start = time.monotonic()
+while True:
+    photo = interrupter.value
+    if photo and not state:
+            counter += 1
+    state = photo
+
+    remaining = max - time.monotonic()
+
+    if remaining <= 0:
+        print("Interrupts:", str(counter))
+        max = time.monotonic() + 4
+        counter = 0
